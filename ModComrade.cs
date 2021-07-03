f18475647200428066b2d473447e802029f4c250
EBuley <65394050+EBuley@users.noreply.github.com>
2021-07-03 16:23:52 +0100

    Add files via upload

---
+++ ModComrade.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+
+namespace SubnauticaModding
+{
+    class ModComrade
+    {
+    }
+}
+[HarmonyPatch(typeof(Knife))]
+[HarmonyPatch("Start")]
+internal class KnifeDamageMod
+{
+    [HarmonyPostfix]
+    public static void Postfix(Knife __instance)
+    {
+        float knifeDamage = __instance.damage;
+        float newKnifeDamage = knifeDamage * 2;
+        __instance.damage = newKnifeDamage;
+        Logger.Log(Logger.Level.Debug, $"Knife damage was: {knifeDamage}," +
+            $"is now {newKnifeDamage}");
+    }
+}
---
+++ QMod.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using HarmonyLib;
+using QModManager.API.ModLoading;
+using Logger = QModManager.Utility.Logger;
+
+namespace SubnauticaModding
+{
+    public class QMod
+    {
+    }
+}
+[QModPatch]
+public static void Patch()
+{
+    var assembly = Assembly.GetExecutingAssembly();
+    var SubnauticaModding = ($"Strands_{assembly.GetName().name}");
+    Logger.Log(Logger.Level.Info, $"Patching {SubnauticaModding}");
+    Harmony harmony = new Harmony(SubnauticaModding);
+    harmony.PatchAll(assembly);
+    Logger.Log(Logger.Level.Info, "Patched Successfully");
+}
+var SubnauticaModding = ($"Strands{Assembly.GetName().name}");
